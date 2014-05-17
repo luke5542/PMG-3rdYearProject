a1db07a5bab90349cb7d1d0f7cd3d21952a25ec3
Devin R <devin.ridgway@gmail.com>
2014-05-17 22:49:10 +0100

    Added several initial files to the git setup

---
+++ game/pmgCrawler/source/app.d
@@ -0,0 +1,16 @@
+import std.stdio;
+
+void main()
+{
+	writeln("Edit source/app.d to start your project.");
+}
+
+
+unittest
+{
+
+	writeln("Testing the PMG Crawler game.");
+
+	assert(1 == 1);
+
+}

6e47b40ee690ed71d3e85ddac133df40b1187182
Devin Ridgway <devin.ridgway@student.manchester.ac.uk>
2014-06-29 19:53:33 -0700

    Built a base test for the Game of Life simulation

--- game/pmgCrawler/source/map.d
+++ game/pmgCrawler/source/map.d
@@ -1,6 +1,14 @@
 module ridgway.pmgcrawler.map;
 
-class MapData
+import std.random;
+
+import dsfml.graphics.sprite;
+import dsfml.system.vector2;
+import dsfml.graphics.renderwindow;
+
+import ridgway.pmgcrawler.tile;
+
+/*class MapData
 {
 
 	private
@@ -13,12 +21,76 @@ class MapData
 		tileTypes = new char[x][y];
 	}
 
-}
+}*/
 
 class TileMap
 {
 	private
 	{
-		Sprite[][] m_tiles;
+		ColoredTile[][] m_tiles;
+
+        Vector2i m_size;
+
+        int m_tileSize;
 	}
+
+    this(Vector2i size, int tileSize = 10)
+    {
+        m_tileSize = tileSize;
+        m_size = size;
+
+        m_tiles = new ColoredTile[][](m_size.x, m_size.y);
+        //m_tiles.length = m_size.y;
+
+        foreach(uint h, ColoredTile[] h_tiles; m_tiles)
+        {
+            //h_tiles.length = m_size.x;
+            foreach(uint w, ColoredTile w_tile; h_tiles)
+            {
+                m_tiles[w][h] = new ColoredTile(Vector2f(m_tileSize, m_tileSize), uniform!(ushort)()%4 == 1);
+                m_tiles[w][h].position = Vector2f(m_tileSize*w, m_tileSize*h);
+            }
+        }
+    }
+
+    const(uint) getHeight()
+    {
+        return m_size.y;
+    }
+
+    const(uint) getWidth()
+    {
+        return m_size.x;
+    }
+
+    bool getIsAlive(uint x, uint y)
+    {
+        return m_tiles[x][y].isAlive;
+    }
+
+    void setIsAlive(uint x, uint y, bool alive)
+    {
+        m_tiles[x][y].isAlive = alive;
+    }
+
+    void setIsStillAlive(uint x, uint y, bool alive)
+    {
+        m_tiles[x][y].isStillAlive = alive;
+    }
+
+    void updateLifeState(uint x, uint y)
+    {
+        m_tiles[x][y].isAlive = m_tiles[x][y].isStillAlive;
+    }
+
+    void draw(ref RenderWindow window)
+    {
+        foreach(ColoredTile[] h_tiles; m_tiles)
+        {
+            foreach(ColoredTile w_tile; h_tiles)
+            {
+                window.draw(w_tile);
+            }
+        }
+    }
 }

631c45583d4a4ede79c86587c82a7b5178a8e8e8
Devin Ridgway <devin.ridgway@student.manchester.ac.uk>
2014-08-16 22:56:15 -0700

    added the baseline structure for the animation framework

---
+++ game/pmgCrawler/source/animation.d
@@ -0,0 +1,39 @@
+module ridgway.pmgcrawler.animation;
+
+import dsfml.system;
+
+class Animation
+{
+	private
+	{
+		Time m_duration;
+	}
+
+	this(Time duration)
+	{
+		m_duration = duration;
+	}
+
+	/// This is called with the value (0-1) of the
+	/// amount that this animation has completed by. TODO: word better
+	abstract void update(float progress);
+
+	final void update(Time time)
+	{
+		//TODO finish this
+	}
+}
+
+class TransformAnimation : Animation
+{
+	private
+	{
+		Transformable m_transformable;
+	}
+
+	this(Transformable transformable, Time duration)
+	{
+		super(duration);
+		m_transformable = transformable;
+	}
+}
